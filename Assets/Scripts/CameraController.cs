86ab6f55e6337b3c71aae6c9cbd5e2c9b6413aa1
Abhishek <abhishekm942@gmail.com>
2021-06-20 05:15:54 +0530

    added camera vertical motion with smoothing

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Base;
+    public Transform defaultPosition;
+    public float CamSpeed = 7.5f;
+    public float sensitivity = 5f;
+    public float maxYaw = 0;
+    public float minYaw = -90;
+    public bool invert = false;
+
+    Vector3 camInitialPos;
+    float currYaw;
+    float yaw;
+    float pitch;
+    float roll;
+
+    void Start()
+    {
+        camInitialPos = defaultPosition.transform.position;
+        yaw = defaultPosition.transform.rotation.eulerAngles.x;
+        pitch = defaultPosition.transform.rotation.eulerAngles.y;
+        roll = defaultPosition.transform.rotation.eulerAngles.z;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            yaw += Input.GetAxis("Mouse Y") * sensitivity * (invert ? 1f : -1f);
+            yaw = Mathf.Clamp(yaw, minYaw, maxYaw);
+        }
+
+        currYaw = Mathf.Lerp(currYaw, yaw, CamSpeed * Time.deltaTime);
+        transform.position = Base.transform.GetChild(GameManager.instance.selectedCarIndex).position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(currYaw, pitch, roll), CamSpeed * Time.deltaTime);
+        transform.Translate(new Vector3(0, 0, -Vector3.Distance(Base.transform.GetChild(GameManager.instance.selectedCarIndex).position, camInitialPos)));
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Car[] cars;
+    [HideInInspector]
+    public int selectedCarIndex;
+    int lastSelectedCarIndex;
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        ActivateSelectedCar(0);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ActivateSelectedCar(int index)
+    {
+        if (cars[lastSelectedCarIndex] != null)
+        {
+            cars[lastSelectedCarIndex].gameObject.SetActive(false);
+        }
+
+        lastSelectedCarIndex = selectedCarIndex;
+        selectedCarIndex = index;
+
+        cars[selectedCarIndex].gameObject.SetActive(true);
+    }
+}
