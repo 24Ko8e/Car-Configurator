a57541504bee07fe9fe12b4b98491b7a9c56e6e7
Abhishek <abhishekm942@gmail.com>
2021-06-20 22:55:50 +0530

    added zooming

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,31 +5,64 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject Base;
-    public Transform defaultPosition;
+    public Transform cameraPosition;
     public float CamSpeed = 7.5f;
     public float sensitivity = 5f;
     public float maxYaw = 0;
     public float minYaw = -90;
     public bool invert = false;
 
-    Vector3 camInitialPos;
+    public bool SetCameraToDefaultTransform = true;
     float currYaw;
     float yaw;
     float pitch;
     float roll;
+
+    float camDistance;
+    float ZoomLevel;
+    public float camZoomSpeed;
+    public float zoomSensitivity;
+    public float MaxZoom;
+    public float minZoom;
+
     float baseRotX;
 
     void Start()
     {
-        camInitialPos = defaultPosition.transform.position;
-        yaw = defaultPosition.transform.rotation.eulerAngles.x;
-        pitch = defaultPosition.transform.rotation.eulerAngles.y;
-        roll = defaultPosition.transform.rotation.eulerAngles.z;
-        
+        camDistance = Vector3.Distance(Base.transform.GetChild(GameManager.instance.selectedCarIndex).position, transform.position);
+        ZoomLevel = camDistance;
+
+        yaw = cameraPosition.transform.rotation.eulerAngles.x;
+        pitch = cameraPosition.transform.rotation.eulerAngles.y;
+        roll = cameraPosition.transform.rotation.eulerAngles.z;
+
         baseRotX = Base.transform.rotation.eulerAngles.y;
     }
 
     void Update()
+    {
+        if (SetCameraToDefaultTransform)
+        {
+            CameraRotation();
+            CameraZooming();
+        }
+    }
+
+    private void CameraZooming()
+    {
+        ZoomLevel += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        if (ZoomLevel < minZoom)
+        {
+            ZoomLevel = Mathf.Lerp(ZoomLevel, minZoom, 50f * Time.deltaTime);
+        }
+        if (ZoomLevel > MaxZoom)
+        {
+            ZoomLevel = Mathf.Lerp(ZoomLevel, MaxZoom, 50f * Time.deltaTime);
+        }
+        camDistance = Mathf.Lerp(camDistance, ZoomLevel, camZoomSpeed * Time.deltaTime);
+    }
+
+    private void CameraRotation()
     {
         if (Input.GetMouseButton(0))
         {
@@ -42,7 +75,7 @@ public class CameraController : MonoBehaviour
         currYaw = Mathf.Lerp(currYaw, yaw, CamSpeed * Time.deltaTime);
         transform.position = Base.transform.GetChild(GameManager.instance.selectedCarIndex).position;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(currYaw, pitch, roll), CamSpeed * Time.deltaTime);
-        transform.Translate(new Vector3(0, 0, -Vector3.Distance(Base.transform.GetChild(GameManager.instance.selectedCarIndex).position, camInitialPos)));
+        transform.Translate(new Vector3(0, 0, -camDistance));
 
         Base.transform.rotation = Quaternion.Slerp(Base.transform.rotation, Quaternion.Euler(0, baseRotX, 0), CamSpeed * Time.deltaTime);
     }
