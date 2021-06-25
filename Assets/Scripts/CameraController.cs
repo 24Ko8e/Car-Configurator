2183a098c5103b082402d203ec40f519f489022a
Abhishek <abhishekm942@gmail.com>
2021-06-25 05:37:39 +0530

    different views setup

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,10 +17,15 @@ public class CameraController : MonoBehaviour
     public bool invert = false;
 
     public bool CameraSetToDefaultTransform = true;
+    public bool LookAround = false;
     float currYaw;
     float yaw;
     float pitch;
     float roll;
+
+    float interiorLookAroundCurrYaw;
+    float interiorLookAroundYaw;
+    float interiorLookAroundPitch;
 
     float camDistance;
     float ZoomLevel;
@@ -34,6 +39,7 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         camDistance = Vector3.Distance(Base.transform.GetChild(GameManager.instance.selectedCarIndex).position, defaultCameraTransform.transform.position);
+
         ZoomLevel = camDistance;
 
         yaw = defaultCameraTransform.transform.rotation.eulerAngles.x;
@@ -58,8 +64,49 @@ public class CameraController : MonoBehaviour
 
     private void SetCameraToFocusPoint()
     {
-        transform.position = Vector3.Lerp(transform.position, targetCameraPosition.position, CamSpeed * Time.deltaTime);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetCameraPosition.rotation, CamSpeed * Time.deltaTime);
+        if (LookAround)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetCameraPosition.position, CamSpeed * Time.deltaTime);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(targetCameraPosition.rotation.eulerAngles.x + interiorLookAroundCurrYaw,
+                                    targetCameraPosition.rotation.eulerAngles.y + interiorLookAroundPitch, targetCameraPosition.rotation.eulerAngles.z), CamSpeed * Time.deltaTime);
+
+            if (Input.GetMouseButton(0))
+            {
+                interiorLookAroundYaw += Input.GetAxis("Mouse Y") * sensitivity * (invert ? -1f : 1f);
+                interiorLookAroundYaw = Mathf.Clamp(interiorLookAroundYaw, -90, 90);
+                interiorLookAroundPitch -= Input.GetAxis("Mouse X") * sensitivity;
+
+            }
+            //interiorLookAroundCurrYaw = Mathf.Lerp(interiorLookAroundCurrYaw, interiorLookAroundYaw, CamSpeed * Time.deltaTime);
+            interiorLookAroundCurrYaw = interiorLookAroundYaw;
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, targetCameraPosition.position, CamSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetCameraPosition.rotation, CamSpeed * Time.deltaTime);
+        }
+    }
+
+    private void CameraRotation()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            yaw += Input.GetAxis("Mouse Y") * sensitivity * (invert ? 1f : -1f);
+            yaw = Mathf.Clamp(yaw, minYaw, maxYaw);
+
+            baseRotX -= Input.GetAxis("Mouse X") * sensitivity;
+        }
+        currYaw = Mathf.Lerp(currYaw, yaw, CamSpeed * Time.deltaTime);
+
+        defaultCameraTransform.transform.position = Base.transform.GetChild(GameManager.instance.selectedCarIndex).position;
+        defaultCameraTransform.rotation = Quaternion.Euler(currYaw, pitch, roll);
+        defaultCameraTransform.transform.Translate(new Vector3(0, 0, -camDistance));
+
+        transform.position = Vector3.Lerp(transform.position, defaultCameraTransform.position, CamSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, defaultCameraTransform.rotation, CamSpeed * Time.deltaTime);
+
+        Base.transform.rotation = Quaternion.Slerp(Base.transform.rotation, Quaternion.Euler(0, baseRotX, 0), CamSpeed * Time.deltaTime);
     }
 
     private void CameraZooming()
@@ -73,36 +120,28 @@ public class CameraController : MonoBehaviour
         {
             ZoomLevel = Mathf.Lerp(ZoomLevel, MaxZoom, 50f * Time.deltaTime);
         }
-        camDistance = Mathf.Lerp(camDistance, ZoomLevel, camZoomSpeed * Time.deltaTime);
+        //camDistance = Mathf.Lerp(camDistance, ZoomLevel, camZoomSpeed * Time.deltaTime);
+        camDistance = ZoomLevel;
     }
 
-    private void CameraRotation()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            yaw += Input.GetAxis("Mouse Y") * sensitivity * (invert ? 1f : -1f);
-            yaw = Mathf.Clamp(yaw, minYaw, maxYaw);
-
-            baseRotX -= Input.GetAxis("Mouse X") * sensitivity;
-        }
-
-        currYaw = Mathf.Lerp(currYaw, yaw, CamSpeed * Time.deltaTime);
-        transform.position = Base.transform.GetChild(GameManager.instance.selectedCarIndex).position;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(currYaw, pitch, roll), CamSpeed * Time.deltaTime);
-        transform.Translate(new Vector3(0, 0, -camDistance));
-
-        Base.transform.rotation = Quaternion.Slerp(Base.transform.rotation, Quaternion.Euler(0, baseRotX, 0), CamSpeed * Time.deltaTime);
-    }
 
     public void SetCustomCameraTransform(Transform target)
     {
         CameraSetToDefaultTransform = false;
         targetCameraPosition = target;
+        ResetInteriorCameraRotation();
     }
 
     public void SetDefaultCameraPosition()
     {
         CameraSetToDefaultTransform = true;
         targetCameraPosition = defaultCameraTransform;
+        ResetInteriorCameraRotation();
+    }
+
+    void ResetInteriorCameraRotation()
+    {
+        interiorLookAroundYaw = 0;
+        interiorLookAroundPitch = 0;
     }
 }
--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -8,48 +8,91 @@ public class Car : MonoBehaviour
 
     public Transform FrontView;
     public Transform RearView;
-    public Transform RightWindow;
-    public Transform LeftWindow;
-    public Transform FrontRightWheel;
-    public Transform FrontLeftWheel;
+    public Transform RightWindowView;
+    public Transform LeftWindowView;
+    public Transform FrontRightWheelView;
+    public Transform FrontLeftWheelView;
+    public Transform InteriorPassengerView;
+    public Transform InteriorDriverView;
+
+    public GameObject defaultViewBtns;
+    public GameObject rightWindowViewBtns;
+    public GameObject leftWindowViewBtns;
+    public GameObject frontViewBtns;
+    public GameObject rearViewBtns;
+    public GameObject interior_P_ViewBtns;
+    public GameObject interior_D_ViewBtns;
+
+    GameObject lastselected;
 
     void Start()
     {
-        
+        lastselected = defaultViewBtns;
     }
 
-    void Update()
+    public void SetCameraToDefaultView()
     {
-        
+        cameraController.SetDefaultCameraPosition();
+        switchBtnSet(defaultViewBtns);
+        cameraController.LookAround = false;
     }
 
     public void SetCameraToFrontView()
     {
         cameraController.SetCustomCameraTransform(FrontView);
+        switchBtnSet(frontViewBtns);
+        cameraController.LookAround = false;
     }
 
     public void SetCameraToRearView()
     {
         cameraController.SetCustomCameraTransform(RearView);
+        switchBtnSet(rearViewBtns);
+        cameraController.LookAround = false;
     }
 
     public void SetCameraToRightWindow()
     {
-        cameraController.SetCustomCameraTransform(RightWindow);
+        cameraController.SetCustomCameraTransform(RightWindowView);
+        switchBtnSet(rightWindowViewBtns);
+        cameraController.LookAround = false;
     }
 
 
     public void SetCameraToLeftWindow()
     {
-        cameraController.SetCustomCameraTransform(LeftWindow);
+        cameraController.SetCustomCameraTransform(LeftWindowView);
+        switchBtnSet(leftWindowViewBtns);
+        cameraController.LookAround = false;
     }
     public void SetCameraToFrontRightWheel()
     {
-        cameraController.SetCustomCameraTransform(FrontRightWheel);
+        cameraController.SetCustomCameraTransform(FrontRightWheelView);
     }
 
     public void SetCameraToFrontLeftWheel()
     {
-        cameraController.SetCustomCameraTransform(FrontLeftWheel);
+        cameraController.SetCustomCameraTransform(FrontLeftWheelView);
+    }
+
+    public void SetCameraToInteriorPassenger()
+    {
+        cameraController.SetCustomCameraTransform(InteriorPassengerView);
+        switchBtnSet(interior_P_ViewBtns);
+        cameraController.LookAround = true;
+    }
+
+    public void SetCameraToInteriorDriver()
+    {
+        cameraController.SetCustomCameraTransform(InteriorDriverView);
+        switchBtnSet(interior_D_ViewBtns);
+        cameraController.LookAround = true;
+    }
+
+    void switchBtnSet(GameObject btnSet)
+    {
+        lastselected.SetActive(false);
+        btnSet.SetActive(true);
+        lastselected = btnSet;
     }
 }
